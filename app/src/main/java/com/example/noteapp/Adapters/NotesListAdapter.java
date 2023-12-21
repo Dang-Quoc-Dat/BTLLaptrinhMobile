7e3142e44bdfb0432f4836321640f82955935529
dang quoc dat <dangquocdat551@gmail.com>
2023-12-21 11:48:41 +0700

    Tạo Adapter

---
+++ app/src/main/java/com/example/noteapp/Adapters/NotesListAdapter.java
@@ -0,0 +1,53 @@
+package com.example.noteapp.Adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.noteapp.Models.Notes;
+import com.example.noteapp.R;
+
+import java.util.List;
+
+public class NotesListAdapter extends RecyclerView.Adapter<NoteViewHolder>{
+
+    Context context;
+    List<Notes> list;
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+
+class NoteViewHolder extends RecyclerView.ViewHolder {
+
+    CardView notes_container;
+    TextView textView_title, textView_notes, textView_date;
+    ImageView imageView_pin;
+    public NoteViewHolder(@NonNull View itemView) {
+        super(itemView);
+        notes_container = itemView.findViewById(R.id.notes_container);
+        textView_title = itemView.findViewById(R.id.textView_title);
+        textView_notes = itemView.findViewById(R.id.textView_notes);
+        textView_date = itemView.findViewById(R.id.textView_date);
+        imageView_pin = itemView.findViewById(R.id.imageView_pin);
+    }
+}
