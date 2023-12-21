cc895ae4d82d16e5fc8e5be99826dd02cacc7870
dang quoc dat <dangquocdat551@gmail.com>
2023-12-21 12:53:22 +0700

    Tạp NotesListAdapter

--- app/src/main/java/com/example/noteapp/Adapters/NotesListAdapter.java
+++ app/src/main/java/com/example/noteapp/Adapters/NotesListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.noteapp.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -11,29 +12,83 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.noteapp.Models.Notes;
+import com.example.noteapp.NotesClickListener;
 import com.example.noteapp.R;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NotesListAdapter extends RecyclerView.Adapter<NoteViewHolder>{
 
     Context context;
     List<Notes> list;
+    NotesClickListener listener;
+
+    public NotesListAdapter(Context context, List<Notes> list, NotesClickListener listener) {
+        this.context = context;
+        this.list = list;
+        this.listener = listener;
+    }
 
     @NonNull
     @Override
     public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new NoteViewHolder(LayoutInflater.from(context).inflate(R.layout.notes_list, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.textView_title.setText(list.get(position).getTitle());
+        holder.textView_title.setSelected(true);
 
+        holder.textView_notes.setText(list.get(position).getNotes());
+
+        holder.textView_date.setText((list.get(position).getDate()));
+        holder.textView_date.setSelected(true);
+
+        if(list.get(position).getPinned()){
+            holder.imageView_pin.setImageResource(R.drawable.ic_pin);
+        }
+        else {
+            holder.imageView_pin.setImageResource(0);
+        }
+
+        int color_code = getRamdomColor();
+        holder.notes_container.setCardBackgroundColor(holder.itemView.getResources().getColor(color_code, null));
+        holder.notes_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onClick(list.get(holder.getAdapterPosition()));
+            }
+        });
+        holder.notes_container.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                listener.onLongClick(list.get(holder.getAdapterPosition()), holder.notes_container);
+                return true;
+            }
+        });
+    }
+
+    private int getRamdomColor(){
+        List<Integer> colorCode = new ArrayList<>();
+
+        colorCode.add(R.color.color1);
+        colorCode.add(R.color.color2);
+        colorCode.add(R.color.color3);
+        colorCode.add(R.color.color4);
+        colorCode.add(R.color.color5);
+
+        Random random = new Random();
+        int random_color = random.nextInt(colorCode.size());
+
+        return random_color;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 }
 
